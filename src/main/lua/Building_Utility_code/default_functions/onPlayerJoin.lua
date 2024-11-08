2d054732248562b40d4cdf4acb0a15c7e3288a48
Rex5522 <169084440+Rex5522@users.noreply.github.com>
2024-11-08 12:23:46 -0800

    added a message to alert the player of the help command during join

--- src/main/lua/Building_Utility_code/default_functions/onPlayerJoin.lua
+++ src/main/lua/Building_Utility_code/default_functions/onPlayerJoin.lua
@@ -1,7 +1,7 @@
 function onPlayerJoin(steamID, name, userID, admin, auth)
     BU_Debug("start of onplayerjoin", steamID, name, userID, admin, auth)
     steamID = tostring(steamID)
-    
+
     if steamID == "0" then
         G_ServerSettings.dedicatedServer = true
         BU_Debug("dedicated server default user joined")
@@ -13,15 +13,17 @@ function onPlayerJoin(steamID, name, userID, admin, auth)
     if not user then
         BU_Debug("user not found. creating user")
         user = userClass:createUser(steamID, name, userID)
+        user:display("welcome to " .. G_ScriptName .. "!\ntype ?help for a list of commands\n(pssst! use ?? to delspawn your newest vehicle)", true)
+        user:display("welcome to " .. G_ScriptName .. "!\ntype ?help for a list of commands\n(pssst! use ?? to delspawn your newest vehicle)", true, true)
     else
         BU_Debug("user found")
     end
-    
+
     if not user then
         BU_Debug("unable to create user")
         return
     end
-    
+
     delayCommand(function() userCommands:fixName(user) end, 500)
     user.ID = userID
     g_savedata.steamIDs[userID] = steamID
