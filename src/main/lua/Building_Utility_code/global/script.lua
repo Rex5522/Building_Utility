527f352226c678c1f19489bfdbba999827948e85
Rex5522 <169084440+Rex5522@users.noreply.github.com>
2024-11-08 12:01:13 -0800

    updated version number pre-steam workshop update

--- src/main/lua/Building_Utility_code/global/script.lua
+++ src/main/lua/Building_Utility_code/global/script.lua
@@ -4,5 +4,5 @@ G_Updated = G_ScriptName.."'s has updated. if you find any issues please report 
 G_BuildType = "DevBuild"
 
 
-G_VersionNumber = "1.02"
+G_VersionNumber = "1.03"
 G_Version = G_ScriptName..G_VersionNumber
