8bf7c04a6ef286c946591bc93350953376dda3ca
Rex5522 <169084440+Rex5522@users.noreply.github.com>
2024-11-14 00:25:08 -0800

    fixed a bug where locations teleported to by vehicles would move up 5 meters per teleport

--- src/main/lua/Building_Utility_code/custom_functions/table/matrix/moveMatrix.lua
+++ src/main/lua/Building_Utility_code/custom_functions/table/matrix/moveMatrix.lua
@@ -5,9 +5,10 @@
 ---@param y integer
 ---@return any
 function moveMatrix(matrix, x, vertical, y)
-    matrix[13] = matrix[13] + x
-    matrix[14] = matrix[14] + vertical
-    matrix[15] = matrix[15] + y
+    local newMatrix = deepCopy(matrix)
+    newMatrix[13] = matrix[13] + x
+    newMatrix[14] = matrix[14] + vertical
+    newMatrix[15] = matrix[15] + y
 
-    return matrix
+    return newMatrix
 end
