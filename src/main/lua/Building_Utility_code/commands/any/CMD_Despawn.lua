205c86c2383a63688a8d38dab868ab2f66e9d0af
Rex5522 <169084440+Rex5522@users.noreply.github.com>
2024-11-22 23:18:01 -0800

    Update CMD_Despawn.lua
    fixed a bug that occurs when trying to despawn a target using ?despawn and added more responsiveness to the command

--- src/main/lua/Building_Utility_code/commands/any/CMD_Despawn.lua
+++ src/main/lua/Building_Utility_code/commands/any/CMD_Despawn.lua
@@ -2,18 +2,37 @@
 -- @param data
 -- @return nil
 function CMD_Despawn(data)
+    local formatCommandData = {
+        command = "?despawn",
+        argData = {
+            [1] = {
+                argName = "vehicleID_Or_GroupID_To_Despawn",
+                Description = "the id of the target you wish to despawn",
+                exampleValue = "82",
+                required = true
+            }
+        }
+    }
+
     local user = data.user
     local vehicleOrGroupToDespawn = data.arg1
 
+    if not vehicleOrGroupToDespawn or vehicleOrGroupToDespawn == "help" then
+        createCommandFormat(formatCommandData, user)
+        return
+    end
 
     local ownerID = -1
     local vehicleData = vehicleGroupExist(vehicleOrGroupToDespawn) or vehicleExist(vehicleOrGroupToDespawn)
-    if vehicleData then
-        ownerID = vehicleData.ownerID
+    if not vehicleData then
+        user:display("failed to find vehicle or group id '" .. vehicleOrGroupToDespawn .. "'", false)
+        return
     end
 
+    ownerID = vehicleData.ownerID
+
     if ownerID ~= user.ID and not data.admin then
-        user:display("you do not own the targeted vehicleID: "..vehicleOrGroupToDespawn, false)
+        user:display("you do not own the targeted vehicleID: "..tostring(vehicleOrGroupToDespawn), false)
         return false
     end
 
